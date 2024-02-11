78846003495b06551b30a841c85f7363f87ffc53
TonyMyrato <tonymyrato@TonyMyratos-Air.MyMiFi>
2024-02-11 00:45:54 -0800

    js-tasks

---
+++ problems/001-sum.js
@@ -0,0 +1,32 @@
+/**
+ * Напишите функцию sum(n) возвращающую положительное целое число – сумму чисел,
+ * расположенных между 1 и N включительно.
+ *
+ * Пример:
+ *
+ * sum(0) === 1
+ * sum(5) === 15
+ *
+ * Больше примеров в тестах
+ *
+ * @param {number} n целое число
+ * @returns {number}
+ */
+
+function sum(n) {
+    if (n <= 0) return 1
+    let sum = 0
+
+    for (let i = 0; i <= n; i++) {
+
+        sum += i
+
+    }
+
+    return sum;
+}
+
+module.exports = sum;
+
+
+console.log(sum(5))
